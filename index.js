9cdaea226e603bc5827a5e929b792b3b25289c11
alexhandaja <handa944@newschool.edu>
2022-11-13 17:29:58 -0500

    init

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hi!")
+import { WeverseClient } from "weverse";
+
+const myClient = new WeverseClient({token: 'my-access-token'})
+myClient.init({allPosts: true, allNotifications: false})
+
+myClient.on('init', async (ready) => {
+    if (ready) {
+        myClient.listen({listen: true, interval: 5000})
+    }
+})
+
+myClient.on('comment', (comment, post) => {
+    // all objects are typed
+    const commenter = myClient.artistById(comment.artist.id)
+    const postAuthor = myClient.artistById(post.artist.id)
+    console.log(`${commenter.name} commented on ${postAuthor.name}'s post!`)
+})
+
+myClient.on('post', (post) => {
+    if (post.photos.length) {
+        post.photos.forEach(photo => {
+            downloadImage(photo.orgImgUrl)
+        })
+    }
+})

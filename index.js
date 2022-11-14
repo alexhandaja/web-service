44ec3994fa23266e4be97dc94f825826e305ad42
alexhandaja <handa944@newschool.edu>
2022-11-14 17:45:04 -0500

    commit

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
+// Import the express library and assign it to a variable
 import { WeverseClient } from "weverse";
-
 const myClient = new WeverseClient({token: 'my-access-token'})
-myClient.init({allPosts: true, allNotifications: false})
+import express from 'express'
+import fetch from 'node-fetch'
 
-myClient.on('init', async (ready) => {
-    if (ready) {
-        myClient.listen({listen: true, interval: 5000})
+// Create an instance of an express application 
+const app = express()
+app.use(express.json())
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/community', (req, res) => {
+
+  myClient.communities.forEach(community => {
+    // typesafe objects with autocompletion
+    const details = {
+        name: community.name,
+        posts: community.posts.length
     }
+}
+  )})
+
+app.get('/name/:name', (req, res) => {
+  console.log(req.params)
+  res.json({ data: "idk this is name." })
 })
 
-myClient.on('comment', (comment, post) => {
-    // all objects are typed
-    const commenter = myClient.artistById(comment.artist.id)
-    const postAuthor = myClient.artistById(post.artist.id)
-    console.log(`${commenter.name} commented on ${postAuthor.name}'s post!`)
+// Example of an application route that makes a request to another server
+app.get('/advice', async (req, res) => {
+  // Make a request to another wbesite and wait for a response
+  const response = await fetch('https://api.adviceslip.com/advice')
+
+  // Read the response
+  const body = await response.json()
+
+  // Print the repsonse body to the console
+  console.log(body)
+
+  // Get the advice text string from the response body object
+  const advice = body.slip.advice
+
+  res.json({ data: advice })
 })
 
-myClient.on('post', (post) => {
-    if (post.photos.length) {
-        post.photos.forEach(photo => {
-            downloadImage(photo.orgImgUrl)
-        })
-    }
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
